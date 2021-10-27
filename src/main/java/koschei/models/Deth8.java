a20e2c001f79c2ac9e7d2c6434369f1e7a4737cd
Evgeniy Kazban <evgeny.kazban3.0@yandex.ru>
2021-10-27 23:11:05 +0300

    init

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,12 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+    private Egg6 egg;
+
+    public Deth8(Egg6 egg) {
+        this.egg = egg;
+    }
 }
